6a349b3e771bcb51c37ceb29c5c35ebbd8052aa4
whosee-12 <157669351+whosee-12@users.noreply.github.com>
2024-01-25 05:23:00 +0000

    taskshett-1-1-5

--- tasksheet_1_1_5.java
+++ tasksheet_1_1_5.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class tasksheet_1_1_5 {
     public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
 
-        String inputString = "radar";
+        // Get the input string from the user
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
 
         // Check if the input string is a palindrome
         if (isPalindrome(inputString)) {
@@ -9,6 +15,9 @@ public class tasksheet_1_1_5 {
         } else {
             System.out.println("The input string is not a palindrome.");
         }
+
+        // Close the scanner
+        scanner.close();
     }
 
     // Function to check if a string is a palindrome
